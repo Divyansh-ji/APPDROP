d9c251d2c3d54dc15f0d8e01ca53c42e184ea08d
Divyansh-ji <Divyanshtiwary01@gmail.com>
2026-02-07 20:25:55 +0530

    app db 2

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
-CREATE EXTENSION IF NOT EXISTS "uuid_ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE pages (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name TEXT NOT NULL,
-  route TEXT UNIQUE NOT NULL,
-  is_home BOOLEAN DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    route TEXT UNIQUE NOT NULL,
+    is_home BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE widgets (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  page_id UUDI NOT NULL REFERENCES page(id) ON DELETE CASCADE,
-  type TEXT NOT NULL,
-  position INT NOT NULL,
-  config JSNOB
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    page_id UUID NOT NULL REFERENCES pages(id) ON DELETE CASCADE,
+    type TEXT NOT NULL,
+    position INT NOT NULL,
+    config JSONB,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
